2f372db2439b3a2d63d4e659467e6606e4ac8f6b
satyam <satyamsurya1997@gmail.com>
2022-03-08 11:03:03 +0530

    routes

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  btnClick() {
+    this.router.navigateByUrl('/user');
+    console.log("clicked");
+};
 
 }
